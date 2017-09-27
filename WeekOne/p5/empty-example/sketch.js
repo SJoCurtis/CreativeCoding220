502e98e268335a2b395832a0d707d5e749f0115c
Sierra Jo Curtis <SJoCurtis@users.noreply.github.com>
2017-09-27 09:42:31 -0600

    y

--- WeekOne/p5/empty-example/sketch.js
+++ WeekOne/p5/empty-example/sketch.js
@@ -1,13 +1,39 @@
-Mover mover;
+var attractors = [];
+var particles = [];
 
-void setup() {
-  size(640,360);
-  mover = new Mover();
+function setup() {
+  createCanvas(400, 400);
+
+  // for (var i = 0; i < 10; i++) {
+  //   attractors.push(createVector(random(width), random(height)));
+  // }
 }
 
-void draw() {
-  background(255);
-  mover.update();
-  mover.checkEdges();
-  mover.display();
+function mousePressed() {
+  attractors.push(createVector(mouseX, mouseY));
+}
+
+function draw() {
+  background(51);
+  stroke(255);
+  strokeWeight(4);
+  particles.push(new Particle(random(width), random(height)));
+
+  if (particles.length > 100) {
+    particles.splice(0, 1);
+  }
+
+  for (var i = 0; i < attractors.length; i++) {
+    stroke(0, 255, 0);
+    point(attractors[i].x, attractors[i].y);
+  }
+  for (var i = 0; i < particles.length; i++) {
+    var particle = particles[i];
+    for (var j = 0; j < attractors.length; j++) {
+      particle.attracted(attractors[j]);
+    }
+    particle.update();
+    particle.show();
+  }
+
 }
