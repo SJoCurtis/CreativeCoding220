74ef7f7a03064396aeaf739eb65f6e959800e6f7
Sierra Jo Curtis <SJoCurtis@users.noreply.github.com>
2017-10-12 14:20:34 -0600

    fff

--- HWTwo/p5/empty-example/sketch.js
+++ HWTwo/p5/empty-example/sketch.js
@@ -7,18 +7,14 @@ function setup() {
 }
 function draw() {
   noFill();
-
   var x = r * cos(a);
   var y = r * sin(a);
-
   a += 0.4;
   r += 0.4;
-
   push();
   translate(width / 2, height / 2);
   strokeWeight(4);
   stroke(r%255);
   ellipse(x, y, 40, 40);
   pop();
-
 }
